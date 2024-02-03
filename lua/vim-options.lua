a1a79c21b32655215816360cb6cac4d56d332ec9
Francisco Ferraz <francisco3ferraz@gmail.com>
2024-02-03 22:21:52 +0000

    Init and basic setup for go

---
+++ lua/vim-options.lua
@@ -0,0 +1,24 @@
+-- Options --
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.nu = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+
+-- Remaps --
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
