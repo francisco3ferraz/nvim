ffdfe558fbc580e0bd40e1a8e938ee435444d318
Francisco Ferraz <francisco3ferraz@gmail.com>
2024-02-03 22:31:51 +0000

    Fix

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,18 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-                null_ls.builtins.code_actions.gomodifytags,
-                null_ls.builtins.code_actions.impl,
-                null_ls.builtins.formatting.gofumpt,
-                null_ls.builtins.formatting.goimports,
-			},
-		})
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.code_actions.gomodifytags,
+                    null_ls.builtins.code_actions.impl,
+                    null_ls.builtins.formatting.gofumpt,
+                    null_ls.builtins.formatting.goimports,
+                },
+            })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end,
+    }
 }
